d165e721091898597f83dbfcf0e0c0f4c65f2f6c
Justin Martenstein <jmartenstein@gmail.com>
2016-07-18 21:17:38 -0700

    Add new data source parameter

--- gather_data.rb
+++ gather_data.rb
@@ -34,22 +34,29 @@ end
 
 options = OpenStruct.new
 options.data_type = :pricing
+options.data_source = :pbo
 
 OptionParser.new do |opts|
   opts.banner = "Usage: gather_data.rb [options]"
   opts.on("-d", "--data [TYPE]", [:pricing, :actuals, :forecast],
-          "Set data type (pricing, forecast, actuals") do |d|
+          "Set data type (pricing, forecast, actuals)") do |d|
     options[:data_type] = d
+  end
+  opts.on("-s", "--source [TYPE]", [:pbo, :fml, :bom],
+          "Set data source (pbo, fml, bom)") do |f| 
+    options[:data_format] = f
   end
   opts.on("-u", "--url [address]", "Set an optional URL for importing") do |u|
     options[:url] = u
   end
 end.parse!
-abort "Incorrect type specificed" if options.data_type.nil? 
+
+abort "Incorrect data type specificed" if options.data_type.nil?
+abort "Incorrect data source specified" if options.data_source.nil?
 
 # set parameters for each type of data
 if options.data_type == :pricing
-  url       = 'http://fantasymovieleague.com/researchvault'
+  url       = 'https://fantasymovieleague.com/researchvault'
   xpath     = "//table[@class='tableType-group hasGroups']//tr"
   sub_xpath = "./td/div//span/span"
 elsif options.data_type == :forecast
